6845b8a3621d47949cc9192ca02f474252dad74a
Ietsiee <309194441+Ietsiee@users.noreply.github.com>
2026-08-03 15:22:32 +0200

    Added build-mango.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -23,6 +23,7 @@ echo "Setup done!"
 echo
 echo "To build linux run: sh ~/GreenOS/scripts/build-linux.sh"
 echo "To build busybox run: sh ~/GreenOS/scripts/build-busybox.sh"
+echo "To build busybox run: sh ~/GreenOS/scripts/build-mango.sh"
 echo
 echo "To build the iso run: sh ~/GreenOS/scripts/build-iso.sh"
-echo "Note: this will only work when you have build linux and busybox"
+echo "Note: this will only work when you have build linux, mango and busybox"
