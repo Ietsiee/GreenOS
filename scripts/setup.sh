4eeba4e866217e66f50e4977960cff4eb85f25d6
Ietsiee <309194441+Ietsiee@users.noreply.github.com>
2026-08-09 11:04:00 +0200

    Cleanup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -14,7 +14,17 @@ sh "$HOME/GreenOS/scripts/e2fsprogs.sh"
 
 sh "$HOME/GreenOS/scripts/busybox.sh"
 
-sh "$HOME/GreenOS/scripts/linux.sh"
+echo "Build kernel now? (y/n)"
+read answer
+
+if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+    echo "Yes"
+    sh "$HOME/GreenOS/scripts/linux.sh"
+else
+    echo "No"
+    echo "sh ~/GreenOS/scripts/linux.sh to build linux"
+    echo "sh ~/GreenOS/scripts/iso.sh to build the iso"
+fi
 
 sh "$HOME/GreenOS/scripts/iso.sh"
 
